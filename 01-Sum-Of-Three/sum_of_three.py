dbd5046e7ad762f9a73dcb19888e175923930b90
Pavel Liser <liser.pavel@gmail.com>
2021-09-01 10:02:11 +0000

    updated challenge solution

--- 01-Sum-Of-Three/sum_of_three.py
+++ 01-Sum-Of-Three/sum_of_three.py
@@ -4,4 +4,4 @@ def sum3(first_number, second_number, third_number):
     """
     Takes three numbers and returns the sum of those numbers
     """
-    return sum([first_number, second_number, third_number])
+    return first_number + second_number + third_number
